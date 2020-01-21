39865cbff10a8bd2cb6d1f7e3ab51c6d32a65cc0
shubhdeeprajput <rajputshubhdeep@gmail.com>
2020-01-21 13:07:01 +0530

    Static

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -39,14 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
-    'whitenoise.runserver_nostatic',
     
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddl-eware'
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -124,11 +122,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, '/static/shop')]
-
-STATICFILES_STORAGE = ['whitenoise.storage.CompressedManifest-StaticFilesStorage']
 
 # managing media
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
